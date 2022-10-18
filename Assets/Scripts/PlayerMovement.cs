31278beff16f7a1ecd2d03e976c6ba59544ff3a4
F-Alex-projects <alex@franquez.com>
2022-10-18 03:04:34 -0400

    Added inputs and what they control
    no pause yet, but everything else (braking, drifting (both set to b on the xbox controller) and accelerating (a)) works on controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
 
-    [SerializeField]float accelerationPower = 5f;
-    [SerializeField]float steeringPower = 5f;
+    [SerializeField]float accelerationPower = 15f;
+    [SerializeField]float steeringPower = 2f;
+    [SerializeField]bool isAccelerating, isBraking, isDrifting;
     float steeringAmount, speed, direction;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isAccelerating = false;
+        isBraking = false;
+        isDrifting = false;
     }
 
     // Update is called once per frame
@@ -27,5 +31,80 @@ public class PlayerMovement : MonoBehaviour
         rb.AddRelativeForce (Vector2.up * speed);
 
         rb.AddRelativeForce (-Vector2.right * rb.velocity.magnitude *steeringAmount/2);
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Accelerate"))
+        {
+            isAccelerating = true;
+        }
+        else
+        {
+            isAccelerating = false;
+        }
+        AccelerationCheck();
+
+        if (Input.GetButton("Brake"))
+        {
+            isBraking = true;
+            BrakeCheck();
+            if (isAccelerating)
+            {
+                isDrifting = true;
+                isBraking = false;
+                DriftCheck();
+            }
+        }
+        else
+        {
+            isBraking = false;
+        }
+    }
+
+    void AccelerationCheck()
+    {
+        if (isAccelerating)
+        {
+            accelerationPower += Time.deltaTime*2f;
+            if (accelerationPower >= 60)
+            {
+                accelerationPower = 60;
+                return;
+            }
+        }
+        else if (!isAccelerating && accelerationPower > 15)
+        {
+            accelerationPower -= Time.deltaTime*5f;
+        }
+        else
+        {
+            accelerationPower = 15;
+        }
+    }
+
+    void BrakeCheck()
+    {
+        if(isBraking)
+        {
+            accelerationPower = 10f;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    void DriftCheck()
+    {
+        if(isDrifting)
+        {
+            steeringPower = 3f;
+        }
+        if (!isAccelerating)
+        {
+            steeringPower = 2f;
+            isDrifting = false;
+        }
     }
 }
